ba18db4de45dcfbd7448633f52088b5607644bc0
Thomas <tbwit078@learner.dvusd.org>
2023-08-14 11:24:46 -0700

    Row Changes
    Added Rows

--- BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -39,18 +36,34 @@ class MainActivity : ComponentActivity() {
 fun Logo() {
     val image = painterResource(R.drawable.android_logo)
     Column(verticalArrangement = Arrangement.Center,) {
-        Image(painter = image, contentDescription = null)
-        Text(text = "          Thomas Witmer",
-            fontSize = 50.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center
-        )
-        Text(text = "Student",
-            fontSize = 24
-                .sp,
-            modifier = Modifier
-                .align(alignment = Alignment.CenterHorizontally))
+        Row(horizontalArrangement = Arrangement.Center,) {
+            Image(painter = image, contentDescription = null)
+        }
+        Row {
+            Text(text = "Thomas Witmer",
+                fontSize = 50.sp,
+                lineHeight = 116.sp,
+                textAlign = TextAlign.Center)
+        }
+        Row(horizontalArrangement = Arrangement.Center,) {
+            Text(
+                text = "Student",
+                fontSize = 24
+                    .sp,
+                //modifier = Modifier.align(alignment = Alignment.CenterHorizontally))
+            )
+        }
     }
+}
+
+@Composable
+fun Info() {
+    Column(verticalArrangement = Arrangement.Center,){
+        Text(text = "Thomas Witmer")
+        Text(text = "")
+        Text(text = "")
+    }
+
 }
 
 @Preview(showBackground = true)
